9ae03bd7b4d117af91787695edfa1151a325bff1
dair370 <dair370@powercam.com.tw>
2021-03-10 17:32:54 +0800

    eesignin.py v1

---
+++ eesignin.py
@@ -0,0 +1,37 @@
+import unittest
+from selenium import webdriver
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+class eesigninCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()   #視窗最大化
+        self.driver.implicitly_wait(3)  #隱式等待3秒
+        self.driver.get("http://eeauto.devtest.tk/")
+
+    def test_eesingin(self):
+        driver = self.driver
+        print(driver.current_url)
+        print(driver.title)
+        WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.PARTIAL_LINK_TEXT, "登入"))) #顯示等待
+        driver.find_element_by_partial_link_text ("登入").click()
+        print(driver.title)
+        driver.find_element_by_name("account").send_keys("admin")
+        driver.find_element_by_name("password").send_keys("111111")
+        driver.find_element_by_class_name("btn-primary").click()
+        print(driver.title)
+        root = driver.find_element_by_partial_link_text("系統管理者").text
+        print(root)
+        self.assertEqual("系統管理者",root,"管理者姓名錯誤") #驗證(expected在前，actual在後)
+        driver.find_element_by_partial_link_text("登出").click()
+        time.sleep(2)
+
+    def tearDown(self):
+        self.driver.close()
+        self.driver.quit()
+
+if __name__ == '__main__':
+     unittest.main(verbosity=2)
