b6e880b562566c3c621c921045c7b3d367d959fc
dair370 <dair370@powercam.com.tw>
2021-03-16 16:27:20 +0800

    eeaddcourse.py v2

--- eeaddcourse.py
+++ eeaddcourse.py
@@ -5,7 +5,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import Select
 
 
@@ -27,16 +26,28 @@ class eeaddcourseCase(unittest.TestCase):
         print(driver.title)
         WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.PARTIAL_LINK_TEXT, "新增")))
         driver.find_element_by_partial_link_text("新增").click()
-        driver.switch_to.frame(0) #用frame的index來定位，第一個是0
+        driver.switch_to.frame(0) #切到子級用frame的index來定位，第一個是0
         WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.NAME, "code")))
         driver.find_element_by_name("code").send_keys(current_time)
         driver.find_element_by_name("name").send_keys(current_time + " 課程")
         driver.find_element_by_name("credit").send_keys("1")
         driver.find_element_by_class_name("select2-search__field").send_keys("admin")
-
-
-        time.sleep(5)
-
+        time.sleep(1) #抓取 select2 需停留1秒
+        driver.find_element_by_class_name("select2-search__field").send_keys(Keys.ENTER) #鍵盤Enter
+        selectGrade = Select(driver.find_element_by_name("gradeId"))
+        selectGrade.select_by_value("10") #select抓value方法
+        for op in selectGrade.options:
+            print(op.text) #取得下拉選單元件裡所有選項的文字
+        selectClass = Select(driver.find_element_by_name("classId"))
+        selectClass.select_by_index(2) #select抓index方法
+        driver.find_element_by_xpath('//*[@id="edit_form"]/div[5]/div/button[1]').click()
+        driver.switch_to.parent_frame() #切到父级frame
+        WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.PARTIAL_LINK_TEXT, "2021/")))
+        driver.find_element_by_partial_link_text("2021/").click() #點剛建立的課程
+        CourseTitle = driver.find_element_by_class_name("fs-text-divider").text
+        print(CourseTitle)
+        self.assertEqual(CourseTitle,current_time + " 課程","兩值不對等")
+        time.sleep(2)
 
     def tearDown(self):
         self.driver.close()
