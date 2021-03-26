3b73a520ef758a5a37f79df1da667ba4234c10fb
dair370 <dair370@powercam.com.tw>
2021-03-26 09:05:05 +0800

    0326 v4

--- eesignup.py
+++ eesignup.py
@@ -14,7 +14,7 @@ class eesignupCase(unittest.TestCase):
 
     def test_eesingup(self):
         driver = self.driver
-        that_day = time.strftime("%Y%m%d%H%M%S", time.localtime(time.time())) #當天日期時間
+        that_day = time.strftime("%Y%m%d%H%M", time.localtime(time.time())) #當天日期時間
         WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.LINK_TEXT, "註冊"))) #顯示等待 30秒內每隔0.5毫秒掃描1次頁面變化
         driver.find_element_by_link_text("註冊").click()
         print(driver.current_url)
